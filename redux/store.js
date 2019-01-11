1ff76b68ad58fd59cf1aa94cf25a7d11368309c5
Alan Bogusiewicz <abogusiewicz@gmail.com>
2019-01-10 18:36:18 -0800

    fixes redux dev tools

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,15 @@
-import { compose, createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import root_reducer from './reducers';
 
-import reducers from './reducers';
+const middleware = [
+  thunk
+];
 
-const middleware = [thunk];
-// eslint-disable-next-line no-underscore-dangle
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(root_reducer, composeWithDevTools(
+  applyMiddleware(...middleware),
+  // other store enhancers if any
+));
 
-export default createStore(reducers, composeEnhancers(applyMiddleware(...middleware)));
+export default store;
